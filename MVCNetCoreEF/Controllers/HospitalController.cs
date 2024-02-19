094d75169e9dcc8028aad93d2e0a8cdc2c176be7
RazvanLucacii <rlucaci48@gmail.com>
2024-02-19 10:26:38 +0100

    Add project files.

---
+++ MVCNetCoreEF/Controllers/HospitalController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCNetCoreEF.Models;
+using MVCNetCoreEF.Repositories;
+
+namespace MVCNetCoreEF.Controllers
+{
+    public class HospitalController : Controller
+    {
+        RepositoryHospital repoHosp;
+
+        public HospitalController(RepositoryHospital repo)
+        {
+            this.repoHosp = repo;
+        }
+        public IActionResult Index()
+        {
+            List<Hospital> hospitales = this.repoHosp.GetHospitales();
+            return View(hospitales);
+        }
+    }
+}
---
+++ MVCNetCoreEF/Data/HospitalContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVCNetCoreEF.Models;
+
+namespace MVCNetCoreEF.Data
+{
+    public class HospitalContext: DbContext
+    {
+        public HospitalContext(DbContextOptions<HospitalContext> options):base(options) 
+        { 
+            
+        }
+
+        public DbSet<Hospital> Hospitales { get; set; }
+    }
+}
